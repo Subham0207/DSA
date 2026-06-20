6fcefacc88001f39e7c1ae2be6c2c3d4c4b278b5
Subham <subhamckp12@gmail.com>
2026-06-20 11:48:07 +0530

    update

---
+++ DSA/Backtracking/Problems/NQueens.js
@@ -0,0 +1,42 @@
+function nQueens(n)
+{
+    const cols = new Set();
+    const posDiag = new Set(); // r + c
+    const negDiag = new Set(); // r - c
+
+    const res = [];
+    const board = Array.from({length:n}, ()=> Array(n).fill('.'));
+
+    function backtrack(r)
+    {
+        if(r === n)
+        {
+            res.push(board.map(r => r.join('')));
+            return;
+        }
+
+        for(let c = 0; c < n;c++)
+        {
+            if(cols.has(c) || posDiag.has(r+c) || negDiag.has(r-c))
+            {
+                continue;
+            }
+
+            cols.add(c);
+            posDiag.add(r+c);
+            negDiag.add(r-c);
+            board[r][c] = 'Q';
+            backtrack(r+1);
+            
+            cols.delete(c);
+            posDiag.delete(r+c);
+            negDiag.delete(r-c);
+            board[r][c] = '.';
+        }
+    }
+
+    backtrack(0);
+    return res;
+}
+
+console.log(nQueens(4));
