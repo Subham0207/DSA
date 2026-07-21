0633acbb3f3de00c4c24b19814d3b80818556f3a
Subham <subham@Subhams-MacBook-Air.local>
2026-07-21 11:06:12 +0530

    update

---
+++ DSA/AdvancedGraph/Practice/findCheapestPrice.js
@@ -0,0 +1,29 @@
+function findCheapestPrice(n, flights, src, dst, k)
+{
+    // Step 1: Initialize distances with infinity
+    prices = new Array(n).fill(Infinity);
+    prices[src] = 0
+
+    // Step 2: Relax edges k + 1 times; k stops means k + 1 flights/edges will be used
+    for(let i =0; i<k+1; i++){
+        temp = prices.copy()  // Snapshot of current prices ( in each level/Breadth )
+        
+        // This loops runs level by level.
+        for(let [u, v, price] in flights)
+        {
+            if(prices[u] == Infinity)  // Only nodes from previous level/depth have valid value other than Inf
+                continue  // Node 'u' hasn't been reached yet
+            
+            // Update destination using the snapshot value of u
+            if(prices[u] + price < temp[v])
+                temp[v] = prices[u] + price
+        }
+        
+        prices = temp  // Update state for the next flight count (once all nodes until current level are processed update prices to advance to next level)
+    }
+
+    // Step 3: Return result
+    if(prices[dst] === Infinity)
+        return -1
+    return prices[dst];
+}
