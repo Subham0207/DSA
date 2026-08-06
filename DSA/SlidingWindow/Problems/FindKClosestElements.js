3c27b6de07f5418471ff92afe60445e8d992bb5b
Subham <subham@Subhams-MacBook-Air.local>
2026-08-06 10:40:12 +0530

    update

--- DSA/SlidingWindow/Problems/FindKClosestElements.js
+++ DSA/SlidingWindow/Problems/FindKClosestElements.js
@@ -1,6 +1,14 @@
 // Question: arr is sorted in asc; 
+// Need to return k elements which satisfy Math.abs(a - x) <= Math.abs(b - x)
+// the result should also be sorted.
 
-// solution: still do not understand it.
+// solution: the fact that the arr is sorted and result is also sorted
+// Only sliding window can implment this in O(n) time complexity.
+// start l at 0 and r at arr.length - 1
+// arr = [1,2,3,4,5], k = 4, x = 3
+// diff= [2,1,0,1,2], l = 0 and r = 4, k = 5
+// iter1: r--, l=0,r=3, k = 4 <-- Answer i.e. [1,2,3,4]
+
 
 function FindKClosestElements(arr, k, x)
 {
