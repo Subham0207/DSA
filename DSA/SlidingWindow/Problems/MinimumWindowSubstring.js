0411c810ca0d6561232d92808e59f4dcde47b185
Subham <subham@Subhams-MacBook-Air.local>
2026-08-10 10:04:08 +0530

    update

--- DSA/SlidingWindow/Problems/MinimumWindowSubstring.js
+++ DSA/SlidingWindow/Problems/MinimumWindowSubstring.js
@@ -1,31 +1,31 @@
+// Solution:
+// using have and need count. And tCount and sCount map.
+// need tracks no of unique characters.
+
 function maxWindowSubString(s,t)
 {
-    function matches(map1,map2)
-    {
-        for(let key in map1)
-        {
-            if((map2[key] || 0) < map1[key]) return false;
-        }
-
-        return true;
-    }
-
     let tCount = {};
     for(let char of t)
     {
         tCount[char] = (tCount[char] || 0) + 1;
     }
-
-    let l =0;
+    let need = Object.keys(tCount).length;
+    let have = 0;
     let sCount = {};
+    let l = 0;
     let minLength = Infinity;
     let res = "";
     for(let r=0;r<s.length;r++)
     {
-        sCount[s[r]] = (sCount[s[r]] || 0) + 1;
+        let char = s[r];
+        sCount[char] = (sCount[char] || 0) + 1;
 
-        // shrink the window until matches since we need the minimum length
-        while(matches(tCount, sCount))
+        if(tCount[char] !== undefined && sCount[char] === tCount[char])
+        {
+            have++;
+        }
+
+        while(have === need)
         {
             let len = r - l + 1;
             if(len < minLength)
@@ -33,11 +33,16 @@ function maxWindowSubString(s,t)
                 minLength = len;
                 res = s.slice(l,r+1);
             }
-            sCount[s[l]]--;
-            l++;
-        }        
-    }
 
+            let charL = s[l];
+            sCount[charL]--;
+            if(tCount[charL] !== undefined  && sCount[charL] < tCount[charL])
+            {
+                have--;
+            }
+            l++;
+        }
+    }
     return res;
 }
 
