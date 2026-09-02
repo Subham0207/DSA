37e9a10e57282ff68897f95980673c8dc45495c1
Subham <subham@Subhams-MacBook-Air.local>
2026-09-02 14:37:11 +0530

    update

---
+++ DSA/LinkedList/Problems/Reorderlist.js
@@ -0,0 +1,13 @@
+// Solution:
+//1. find middle ( using slow and fast pointer), That gives us first half and second half
+//2. reverse the second half
+//3. merge two halfs
+
+function reorderList(head)
+{
+    //find middle ( using slow and fast pointer), That gives us first half and second half
+
+    //reverse the second half
+
+    // merge two halfs
+}
---
+++ Readme.md
@@ -0,0 +1,3 @@
+# DSA Patterns
+
+1. https://github.com/Chanda-Abdul/Several-Coding-Patterns-for-Solving-Data-Structures-and-Algorithms-Problems-during-Interviews/blob/main/%E2%9C%85%20%20Pattern%2003:%20Fast%20&%20Slow%20pointers.md
